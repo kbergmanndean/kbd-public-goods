47c2adb67102e292ad44e8ecdefdebbba9d40e11
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2021-12-17 15:34:55 -0500

    section 1 done

--- src/Components/Page.js
+++ src/Components/Page.js
@@ -2,7 +2,16 @@ import React from "react"
 
 function Page(){
     return(
-        <p>page</p>
+        <div>
+            <h1>Treat your pet (and you). It's on us!</h1>
+            <p>Treat your pet to a healthy treat for <span>free</span> to kick off your Public Goods trial membership.</p>
+            <h2>See what it's like to be a Public Goods member.</h2>
+            <ul>
+                <li>Included in your bundle is a free 2-week membership that gives you unlimited access to our entire collection of sustainable essentials.</li>
+                <li>Take a load off. We make it easy for you to make better choices. Always included: eco-friendly products you can trust.</li>
+            </ul>
+            <button>Claim your free offer</button>
+        </div>
     )
 }
 
